cc860aaea327bb7b00aa3f3f24b821cc142b4ce2
A248 <theanandbeh@gmail.com>
2021-06-13 22:15:38 -0400

    Add adventure to Sign API

--- src/main/java/org/bukkit/block/Sign.java
+++ src/main/java/org/bukkit/block/Sign.java
@@ -1,16 +1,56 @@
 package org.bukkit.block;
 
+import net.kyori.adventure.text.Component;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.List;
+
 /**
  * Represents a captured state of either a SignPost or a WallSign.
  */
+// Solar start - add adventure to whole class
 public interface Sign extends BlockState {
 
     /**
      * Gets all the lines of text currently on this sign.
      *
-     * @return Array of Strings containing each line of text
+     * @return a list of components for each line of text
      */
-    public String[] getLines();
+    @NonNull
+    List<Component> lines();
+
+    /**
+     * Gets the line of text at the specified index. <br>
+     * <br>
+     * For example, getLine(0) will return the first line of text.
+     *
+     * @param index line index to get the text from
+     * @return text on the given line
+     * @throws IndexOutOfBoundsException if the line index does not exist
+     */
+    @NonNull
+    Component line(int index) throws IndexOutOfBoundsException;
+
+    /**
+     * Sets the line of text at the specified index. <br>
+     * <br>
+     * For example, setLine(0, Component.text("Line One")) will set the first line
+     * of text to "Line One".
+     *
+     * @param index line index to set the text at
+     * @param line the new text to place at the given line
+     * @throws IndexOutOfBoundsException if the line index does not exist
+     */
+    void line(int index, @NonNull Component line) throws IndexOutOfBoundsException;
+
+    /**
+     * Gets all the lines of text currently on this sign.
+     *
+     * @return Array of Strings containing each line of text
+     * @deprecated Use the adventure {@link #lines()}
+     */
+    @Deprecated
+    String[] getLines();
 
     /**
      * Gets the line of text at the specified index.
@@ -20,8 +60,10 @@ public interface Sign extends BlockState {
      * @param index Line number to get the text from, starting at 0
      * @throws IndexOutOfBoundsException Thrown when the line does not exist
      * @return Text on the given line
+     * @deprecated Use the adventure {@link #line(int)}
      */
-    public String getLine(int index) throws IndexOutOfBoundsException;
+    @Deprecated
+    String getLine(int index) throws IndexOutOfBoundsException;
 
     /**
      * Sets the line of text at the specified index.
@@ -32,6 +74,9 @@ public interface Sign extends BlockState {
      * @param index Line number to set the text at, starting from 0
      * @param line New text to set at the specified index
      * @throws IndexOutOfBoundsException If the index is out of the range 0..3
+     * @deprecated Use the adventure {@link #line(int, Component)}
      */
-    public void setLine(int index, String line) throws IndexOutOfBoundsException;
+    @Deprecated
+    void setLine(int index, String line) throws IndexOutOfBoundsException;
 }
+// Solar end
