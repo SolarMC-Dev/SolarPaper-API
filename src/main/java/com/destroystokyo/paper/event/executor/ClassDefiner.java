105df15a6873b8efeb6eb2af747074ed64d3fbfd
A248 <theanandbeh@gmail.com>
2021-07-19 14:56:35 -0400

    Add means to determine generated event executor class name

--- src/main/java/com/destroystokyo/paper/event/executor/ClassDefiner.java
+++ src/main/java/com/destroystokyo/paper/event/executor/ClassDefiner.java
@@ -16,10 +16,23 @@ public interface ClassDefiner {
     Class<?> defineClass(Class<?> listenerClass, String name, byte[] data);
 
     /**
+     * Determines the name which will be used for the generated class file data
+     *
+     * @param listenerClass the listener class which the generated class must be able to access
+     * @param uniqueId an integer unique to the generated class which may be used to deduplicate class names
+     * @return the full name of the generated class
+     */
+    default String getDefinedClassName(Class<?> listenerClass, int uniqueId) {
+        return getDefiningPackage() + ".GeneratedEventExecutor" + uniqueId;
+    }
+
+    /**
      * Determines the package in which the generated class file data will derive its class name
      *
      * @return the package in which to place the generated class
+     * @deprecated Implementors should override {@link #getDefinedClassName(Class, int)}  instead
      */
+    @Deprecated
     default String getDefiningPackage() {
         return "com.destroystokyo.paper.event.executor.asm.generated";
     }
